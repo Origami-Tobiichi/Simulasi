1361f02cedebe9e2b50f55d0e6a6c17d8583a345
Origami-Tobiichi <cyberdark454@gmail.com>
2024-12-13 00:02:05 +0800

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,21 @@
+document.getElementById('login-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  fetch('/api/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        window.location.href = '/public/exam.html';
+      } else {
+        document.getElementById('error-message').innerText = 'Username atau password salah!';
+      }
+    })
+    .catch(err => console.error(err));
+});
